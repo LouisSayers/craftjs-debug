e3f9b9a982c2a789d6c199618dcdd41d552f34a5
Louis Sayers <lssayers@gmail.com>
2022-11-09 11:47:01 +1300

    Added CraftOutput to page to see the internal state as it changes

--- src/CraftOutput.tsx
+++ src/CraftOutput.tsx
@@ -1,15 +1,30 @@
-import React, {CSSProperties} from 'react'
+import React, {CSSProperties, useEffect, useState} from 'react'
+import {useEditor} from '@craftjs/core'
 
 export const CraftOutput = () => {
+  const { query } = useEditor()
+  const [output, setOutput] = useState('')
+
   const styles: CSSProperties = {
     background: 'grey',
     gridArea: 'craftoutput',
     padding: '10px',
   }
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let nodeTree = JSON.parse(query.serialize())
+      setOutput(JSON.stringify(nodeTree, null,  2))
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    };
+  }, []);
+
+
   return (
     <div style={styles}>
-      Craft out goes here
+      <pre>{output}</pre>
     </div>
   )
 }
